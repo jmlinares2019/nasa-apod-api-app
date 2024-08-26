dafb1e7f0c944f2b0801c2a0ddff447eb451af8b
jmlinares2019 <jlinaresmar@gmail.com>
2024-08-26 17:51:27 +0200

    Creating and importing Main, Sidebar & Footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer(){
+    return (
+        <>
+            <footer>
+                {/* <div className="bg-gradient"></div> */}
+                <div className="main-info">
+                    <h2>The Brutal Martian Landscape</h2>
+                    <h1>APOD Project</h1>
+                </div>
+                <button className="read-more">
+                    <i className="fa-solid fa-circle-info"></i>
+                </button>
+            </footer>
+            
+        </>
+    )
+}
+
+export default Footer
