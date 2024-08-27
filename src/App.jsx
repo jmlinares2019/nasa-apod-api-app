090e35f10d96eb8935cf7bcd197e408e3fc611eb
jmlinares2019 <jlinaresmar@gmail.com>
2024-08-27 18:31:09 +0200

    Implementing localStorage read / write to cache and retrieve data

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,30 @@ function App() {
     async function fetchAPIData(){
       const API_KEY = 'DNhGCawYAHc4F78tMxypWtDq2ybahAuW32a8pD3O'
       // const API_KEY = import.meta.env.NASA_API_KEY; <-- not working!!!
-      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${API_KEY}`
+      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${API_KEY}`;
+
+      const today = (new Date()).toDateString();
+      const localKey = `NASA-APOD-API-${today}`
+      // if localKey already exist in localStorage ... 
+      if (localStorage.getItem(localKey)){
+        // ... read its value (parsing it as an object)
+        const apiData = JSON.parse(localStorage.getItem(localKey))
+        // and set it as const 'data'
+        setData(apiData);
+        console.log("Fetched from localStorage")
+        console.log(localStorage)
+        return
+      }
+      localStorage.clear();
+
       try{
         const res = await fetch(url)
         const apiData = await res.json()
+        // saving apiData in localStorage with key 'localKey' (as a string)
+        localStorage.setItem(localKey, JSON.stringify(apiData))
         setData(apiData)
         console.log('DATA\n', apiData)
+        console.log("fetched from the API today")
       } catch(err) {
         console.log(err.message)
       }
