2ac9edb8faf798b1602426db941e0bf3438d1faa
jmlinares2019 <jlinaresmar@gmail.com>
2024-08-29 20:23:49 +0200

    Adding my contact details on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,32 @@ function Footer(props){
     return (
         <>
             <footer>
-                {/* <div className="bg-gradient"></div> */}
+                <div className="bg-gradient">
+                    {/* footer decorative gradient background */}
+                </div>
                 <div className="main-info">
-                    <h1 className="main-title">APOD PROJECT</h1>
+                    <h1 className="main-title">
+                        <a 
+                            href="https://apod.nasa.gov/apod/astropix.html" 
+                            target="_blank"
+                            className="project-link">
+                            APOD PROJECT
+                        </a>
+                    </h1>
                     <h2 className="footer-pic-title">{data?.title}</h2>
+                    <div className="contact-me">
+                        <p className="my-name">Juan Manuel Linares</p>
+                        <a href="https://github.com/jmlinares2019" className="github-icon contact-link">
+                            <i className="fa-brands fa-github"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/juan-manuel-linares/" className="linkedin-icon contact-link">
+                            <i className="fa-brands fa-linkedin-in"></i>
+                        </a>
+                    </div>
                 </div>
                 <button 
                     className="read-more"
-                    onClick={props.onHandleClick}>
+                    onClick={onHandleClick}>
                     <i className="fa-solid fa-circle-info"></i>
                 </button>
             </footer>
