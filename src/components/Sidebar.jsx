002ab99b32c9d16c58edd78efcb04a20e35d817a
jmlinares2019 <jlinaresmar@gmail.com>
2024-08-29 20:23:33 +0200

    Changing sidebar's show / hide logic to allow animation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,19 @@
 function Sidebar(props){
-    const { data, onHandleClick } = props
-    return (
-        <>  
-            <div className="sidebar-wrapper">
-                <div className="bg-overlay"></div>
-                <div className="sidebar-content">
-                    <h2 className="sidebar-title">{data?.title}</h2>
-                    <p className="sidebar-date">{data?.date}</p>
-                    <p className="sidebar-description">{data?.explanation}</p>
-                    <button 
-                        className="read-less"
-                        onClick={onHandleClick}>
-                        <i className="fa-solid fa-arrow-right"></i>
-                    </button>
-                </div>
-                
-            </div>
-            
-        </>
+    const { data, onHandleClick, showModal } = props
+    return ( 
+        <div className={showModal ? "sidebar-wrapper show" : "sidebar-wrapper hide"}>
+            <div className="sidebar-content">
+                <h2 className="sidebar-title">{data?.title}</h2>
+                <p className="sidebar-author">&copy; {data?.copyright ? data.copyright : ""}</p>
+                <p className="sidebar-date">{data?.date}</p>
+                <p className="sidebar-description">{data?.explanation}</p>
+                <button 
+                    className="read-less"
+                    onClick={onHandleClick}>
+                    <i className="fa-solid fa-arrow-right"></i>
+                </button>
+            </div>   
+        </div>
     )
 }
 
